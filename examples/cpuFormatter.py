0b421d31f327e8d222be3b63cbf35aa825c8638a
Brian Hawkins <brianhks1@gmail.com>
2015-02-13 15:50:46 -0700

    added formatter for disk and interface

--- examples/cpuFormatter.py
+++ examples/cpuFormatter.py
@@ -2,7 +2,6 @@
 # cpu number and type are set as tags
 # class Formatter:
 
-# For column families, remove plugin_instance from name and change plugin name to "cassandra" and add column_families as a tag
 def format(metric_name_template, tags, hostname, plugin, plugin_instance, type, type_instance):
 	if plugin == 'cpu':
 		metric_name_template = metric_name_template.replace('%(plugin_instance)s.', '')
@@ -10,5 +9,13 @@ def format(metric_name_template, tags, hostname, plugin, plugin_instance, type, 
 		tags['cpu'] = plugin_instance
 		tags['type'] = type_instance
 		type_instance = 'utilization'
+	
+	if plugin == 'disk':
+		metric_name_template = metric_name_template.replace('%(plugin_instance)s.', '')
+		tags['disk'] = plugin_instance
+		
+	if pluing == 'interface':
+		metric_name_template = metric_name_template.replace('%(type)s.', '')
+		tags['interface'] = type_instance
 		
 	return (metric_name_template % {'host': hostname, 'plugin': plugin, 'plugin_instance': plugin_instance, 'type': type, 'type_instance': type_instance}, tags)
